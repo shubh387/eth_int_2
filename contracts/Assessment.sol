0658e4f7fa9291d969a2aa3d3152c8ec7f4a4ed3
shubh387 <shubham387798@gmail.com>
2024-07-12 11:45:31 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -3,58 +3,58 @@ pragma solidity ^0.8.9;
 
 //import "hardhat/console.sol";
 
-contract RatingSystem {
+contract Assessment {
     address payable public owner;
     uint256 public Rating;
 
-    event PositiveRating(uint256 Grade);
-    event NegativeRating(uint256 Grade);
+    event PositiveRating(uint256 Rate);
+    event NegativeRating(uint256 Rate);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
-         Rating= initBalance;
+        Rating = initBalance;
     }
 
     function getBalance() public view returns(uint256){
         return Rating;
     }
 
-    function PositiveRating(uint256 _Grade) public payable {
+    function Positiverating(uint256 _Rate) public payable {
         uint _PreviousRating = Rating;
 
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
         // perform transaction
-        Rating += _Grade;
+        Rating += _Rate;
 
         // assert transaction completed successfully
-        assert(Rating == _PreviousRating + _Grade);
+        assert(Rating == _PreviousRating + _Rate);
 
         // emit the event
-        emit PositiveRating(_Grade);
+        emit PositiveRating(_Rate);
     }
 
     // custom error
-    error InsufficientRating(uint256 Rating, uint256 NegativeRatingGrade);
+    error InsufficientRating(uint256 Rating, uint256 NegativeRatingRate);
 
-    function NegativeRating(uint256 _NegativeRatingGrade) public {
+    function Negativerating(uint256 _NegativeRatingRate) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _PreviousRating = Rating;
-        if (Rating < _NegativeRatingGrade) {
-            revert InsufficientBalance({
+        if (Rating < _NegativeRatingRate) {
+            revert InsufficientRating({
                 Rating: Rating,
-                NegativeRatingGrade: _NegativeRatingGrade
+                NegativeRatingRate: _NegativeRatingRate
             });
         }
 
-        // NegativeRating the given Grade
-        Rating -= _NegativeRatingGrade;
+        // NegativeRating the given Rate
+        Rating -= _NegativeRatingRate;
 
         // assert the Rating is correct
-        assert(Rating == (_PreviousRating - _NegativeRatingGrade));
+        assert(Rating == (_PreviousRating - _NegativeRatingRate));
 
         // emit the event
-        emit NegativeRating(_NegativeRatingGrade);
+        emit NegativeRating(_NegativeRatingRate);
     }
 }
